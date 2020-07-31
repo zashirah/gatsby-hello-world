f3c9e61dabb7d759c277953ddcd22640d0319513
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-07-31 12:09:25 -0400

    updated home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,19 @@ import { Link } from 'gatsby'
 import Header from "../components/header"
 
 function Home() {
-  return <div>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="Hello Gatsby!" />
+  return (
+    <div>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
+      <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>
+          What do I like to do? Lots of course but definitely enjoy building
+          websites.
+        </p>
+      </div>
     </div>
+  )
 }
 
 export default Home
