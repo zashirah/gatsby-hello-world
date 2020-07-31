a7c9edc96f159cd0ecbac4851a722537ea2f2d28
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-07-31 11:30:28 -0400

    adeded gatsby-browser

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from 'gatsby'
-import Header from "./components/header"
+import Header from "../components/header"
 
 export default function Home() {
   return <div>
